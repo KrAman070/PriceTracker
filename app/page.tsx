e5bad390084d13913cee957bd5dc3ee37e0b1db9
Aman <21bee070@nith.ac.in>
2024-09-06 02:01:13 +0530

    First commit

---
+++ app/page.tsx
@@ -0,0 +1,54 @@
+import HeroCarousel from "@/components/HeroCarousel"
+import Searchbar from "@/components/Searchbar"
+import Image from "next/image"
+import { getAllProducts } from "@/lib/actions"
+import ProductCard from "@/components/TrendingProductHome"
+
+const Homes = async () => {
+  const allProducts = await getAllProducts();
+
+  return (
+    <>
+      <section className="px-6 md:px-20 py-24">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center"> 
+            <p className="small-text text-primary-blue">
+              <span className="text-primary-blue">Smart Shopping Starts Here:</span>
+              <Image
+                src="/assets/icons/arrow-right.svg"
+                alt="arrow-right"
+                width={16}
+                height={16}
+              />
+            </p>
+
+            <h1 className="head-text">
+              Be ready to save more on purchase with
+              <span className="text-primary-blue"> PriceTracker</span>
+            </h1>
+
+            <p className="mt-6">
+              Powerful, PriceTracker help you get the best deal by analizing the price of the products. Get notifications on your mail at every price change of the product.
+            </p>
+
+            <Searchbar />
+          </div>
+
+          <HeroCarousel />
+        </div>
+      </section>
+
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {allProducts?.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Homes
---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+"use-client"
+const Footer=()=>{
+    return(
+        <div className="footer text-white flex justify-center items-center bg-black h-[100px] w-full">
+    Designed by <span className="ml-1 text-primary-blue"> Aman</span>
+</div>
+
+    )
+}
+export default Footer;
